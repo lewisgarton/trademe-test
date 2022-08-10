70b87205472f01329df5e5fbf60fc10230690b8c
lewisgarton <lewisgarton@gmail.com>
2022-08-10 15:40:24 +1200

    adds screens to destinations

---
+++ app/src/androidTest/java/com/lewisgarton/trademetest/presentation/screens/discover/listingcard/ListingCardTest.kt
@@ -0,0 +1,90 @@
+package com.lewisgarton.trademetest.presentation.screens.discover.listingcard
+
+import ListingCard
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.lewisgarton.trademetest.service.models.Listing
+import com.lewisgarton.trademetest.ui.theme.Theme
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.math.BigDecimal
+
+class ListingCardTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun setupBefore() {
+        composeTestRule.setContent {
+            Theme {
+                ListingCard(listing = Listing(
+                    title = "Title",
+                    region = "Region",
+                    pictureHref = null,
+                    buyNowPrice = BigDecimal(33),
+                    displayPrice = "$44.50",
+                    isClassified = false
+                )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun containsListingThumbnail() {
+        composeTestRule
+            .onNodeWithTag("listing-thumbnail")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun containsListingTitle() {
+        composeTestRule
+            .onNodeWithText("Title")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun containsRegion() {
+        composeTestRule
+            .onNodeWithText("Region")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun containsPriceRow() {
+        composeTestRule
+            .onNodeWithTag("price-row")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun isClickable() {
+        composeTestRule
+            .onRoot()
+            .assertHasClickAction()
+    }
+
+    @Test
+    fun whenClickedDisplaysToast() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun whenTitleIsLargeTextIsClipped() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun whenTitleIsLargeMaxLinesIsTwo() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun whenRegionIsLargeTextIsClipped() {
+        assertTrue(false)
+    }
+}
--- app/src/main/java/com/lewisgarton/trademetest/presentation/navigation/Destinations.kt
+++ app/src/main/java/com/lewisgarton/trademetest/presentation/navigation/Destinations.kt
@@ -7,6 +7,10 @@ import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.lewisgarton.trademetest.presentation.screens.discover.DiscoverScreen
+import com.lewisgarton.trademetest.presentation.screens.mytrademe.MyTrademeScreen
+import com.lewisgarton.trademetest.presentation.screens.notifications.NotificationsScreen
+import com.lewisgarton.trademetest.presentation.screens.watchlist.WatchlistScreen
 
 interface Destination {
     val icon: ImageVector
@@ -19,7 +23,7 @@ interface Destination {
 object Discover : Destination {
     override val icon: ImageVector = Icons.Filled.Home
     override val route: String = "discover"
-    override val screen: @Composable () -> Unit = { }
+    override val screen: @Composable () -> Unit = { DiscoverScreen() }
     override val label: String = "Discover"
     override val title: String = "Browse"
 }
@@ -27,7 +31,7 @@ object Discover : Destination {
 object Notifications : Destination {
     override val icon: ImageVector = Icons.Filled.Notifications
     override val route: String = "notifications"
-    override val screen: @Composable () -> Unit = { }
+    override val screen: @Composable () -> Unit = { NotificationsScreen() }
     override val label: String = "Notifications"
     override val title: String
         get() = label
@@ -35,7 +39,7 @@ object Notifications : Destination {
 object Watchlist : Destination {
     override val icon: ImageVector = Icons.Filled.Search
     override val route: String = "watchlist"
-    override val screen: @Composable () -> Unit = { }
+    override val screen: @Composable () -> Unit = { WatchlistScreen() }
     override val label: String = "Watchlist"
     override val title: String
         get() = label
@@ -43,7 +47,7 @@ object Watchlist : Destination {
 object MyTrademe : Destination {
     override val icon: ImageVector = Icons.Filled.AccountBox
     override val route: String = "my_trademe"
-    override val screen: @Composable () -> Unit = { }
+    override val screen: @Composable () -> Unit = { MyTrademeScreen() }
     override val label: String = "My Trademe"
     override val title: String
         get() = label
